456c27642a44f5574ff386a7aaa5380301630a72
Maxim Vistunov <max@MacBook-Pro-landlord.local>
2018-02-19 08:49:45 +0700

    Last save

--- chessdec.c
+++ chessdec.c
@@ -1,25 +1,24 @@
-
 #include<stdio.h>
 
 int main() {
-    int i, j;
-    for (i = 1; i <= 8; i++){
-        printf("\n");
-        for (j = 1; j <= 8; j++){
-            if (i % 2 == 0){
-            if (j % 2 == 0){
-                printf("O ");
-            } else {
-                printf("X ");
-            }
-            } else {
-                if (j % 2 == 0){
-                    printf("X ");
-                } else {
-                    printf("O ");
-                }
-            }
+    for (int i = 1; i <= 9; i++){
+        if (i==1){
+            printf("1 r n b q k b n r");
+        }else if(i==8){
+            printf("\n8 R N B Q K B N R");
+        }else if(i==2){
+            printf("\n2 p p p p p p p p");
+        }else if(i==7){
+            printf("\n7 p p p p p p p p");
+        }else if(i==9){
+            printf("\n  a b c d e f g h");
+        }else{
+            printf("\n%d", i);
         }
+        
+        
+        
     }
-    printf("\n \n");
+    printf("\n\n");
 }
+
